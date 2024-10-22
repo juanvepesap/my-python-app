737a56b81d4dd618356466b09b6be25116cc3510
juanvepe <143229813+juanvepesap@users.noreply.github.com>
2024-10-22 16:39:01 -0500

    terraformfiles

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.region
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+}
